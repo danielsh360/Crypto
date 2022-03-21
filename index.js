7db7e0ec6659f2243ffa1055a7fbadb87602c96c
danielsh360 <102035310+danielsh360@users.noreply.github.com>
2022-03-21 14:37:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,159 @@
-html
-body
-/body
-/html
+<html>
+	<head>
+		<title>Daniel Shevchenko portfolio/blog - interior, graphic and web design</title>
+		 <link rel="stylesheet" type="text/css" href="style/index.css">
+		 <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+	<meta property="fb:admins" content="100001332172650" /></head>
+	<header>
+		<div id="about"><a href="about.html"><h3>Hi! I'm Daniel Shevchenko, interior/graphic designer and web developer from Ukraine<h3></a></div>
+		<div id="logo"></div>
+		</div>
+		<a href="https://www.facebook.com/danielshevchenk0"><div id="fb"></div></a>
+		<a href="mailto: danielshevchenko@me.com"><div id="mail"></div></a>
+		
+		
+	</header>
+	<body>
+	
+		
+	
+		<div id="block10">
+			<div id="block10pic"></div>
+			<h1>korona-leva</h1>
+			<h4>They are very nice people and have great accessories for Apple</h4>
+		</div></a>	
+	
+		<a href="http://www.smartbar.com.ua"><div id="block5">
+			<div id="block5pic"></div>
+			<h1>smartbar.com.ua</h1>
+			<h4>Web design</h4>
+		</div></a>
+	
+	
+		<a href="about.html"><div id="block8">
+			<div id="block8pic"></div>
+			<h2>Few words about me</h2>
+			<h5>Really, just four sentences, but they are enough to describe me.</h5>
+		</div></a>
+		
+		<a href="minimal.html"><div id="block9">
+			<div id="block9pic"></div>
+			<h1>Minimalistic house</h1>
+			<h4>My objective was to maximise rid of all unnecessary stuff</h4>
+		</div></a>
+	
+		<a href="autostudio.html"><div id="block1">
+			<div id="block1pic"></div>
+			<h1>Automotive design studio </h1>
+			<h4>My graduation project. Idea was to create automotive design studio of my dream. I think I did it</h4>
+		</div></a>
+		
+		<a href="pub.html"><div id="block6">
+			<div id="block6pic"></div>
+			<h2>Pub "Knights Templar"</h2>
+			<h5>Interior modern interpretation of those times</h5>
+		</div></a>
+		
+		<a href="alexander.html"><div id="block7">
+			<div id="block7pic"></div>
+			<h1>Alexander Shevchenko</h1>
+			<h4>Great artist, great thinker, great dad</h4>
+		</div></a>
+		
+		<a href="bablo.html"><div id="block2">
+			<div id="block2pic"></div>
+			<h2>T-shirts "Bablo"</h2>
+			<h5>Prints showing problem of modern people - being too material</h5>
+		</div></a>
+		
+		<a href="smart.html"><div id="block3">
+			<div id="block3pic"></div>
+			<h1>Smart presentation stand</h1>
+			<h4>University homework in third grade. Task look's simple, but it was not</h4>
+		</div>
+		
+		<a href="smartbar.html"><div id="block4">
+			<div id="block4pic"></div>
+			<h2>Accessories selling booth</h2>
+			<h5>Booth of my friend which you can see in Lviv Auchan</h5>
+		</div></a>
+		
+		<div id="blog">
+			<a href="blog.html"><div id="bloghead"><h7>Blog</h7><hr></div></a>
+			
+			<a href="blog.html"><div id="post1">
+				<div id="blogblock1">
+					<div id="blogblock1photo"></div>
+					<h6>My blog is empty but I will take my laptop and write something interesting soon</h6>
+			</div></a>
+			</div>
+		</div>
+		
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+				ga('create', 'UA-46830700-1', 'danielshevchenko.com');
+				ga('send', 'pageview');
+		</script>
+		
+</body>
+<footer>
+	<div id="interior">
+	
+	<nav>
+		<ul class="top-menu">
+			<ul>Interior design</ul>
+				<li><a href="autostudio.html">Automotive studio</a></li>
+				<li><a href="pub.html">Pub "Knights Templar"</a></li>
+				<li><a href="smartbar.html">Accessories selling booth</a></li>
+				<li><a href="smart.html">Smart presentation stand</a></li>
+				<li><a href="minimal.html">Minimalistic house</a></li>
+		</ul>
+	</nav>
+	
+	</div>
+	<div id="web">
+		
+		<nav>
+		<ul class="top-menu">
+			<ul>Web-design</ul>
+				<li><a href="http://www.smartbar.com.ua">smartbar.com.ua</a></li>
+		</ul>
+		</nav>
+		
+	</div>
+	
+	<div id="fashion">
+		
+		<nav>
+		<ul class="top-menu">
+			<ul>Fashion</ul>
+				<li><a href="bablo.html">T-shirts "Bablo"</a></li>
+		</ul>
+		</nav>
+		
+	</div>
+	
+	<div id="people">
+		
+		<nav>
+		<ul class="top-menu">
+			<ul>People</ul>
+				<li><a href="alexander.html">Alexander Shevchenko</a></li>
+		</ul>
+		</nav>
+		
+	</div>
+	<div id="footerblock1">
+		<li>Daniel Shevchenko. From 1993 till now</li>
+	</div>
+	<div id="footerblock2">
+		<a href="about.html"><li>About</li></a>
+	</div>
+	<div id="footerlogo"></div>
+	
+	</footer>
+</html>
